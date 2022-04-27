73dd8e405c7ec6b21fcdedbf4ec2e61464c63abd
Jmoreno844 <88803941+Jmoreno844@users.noreply.github.com>
2022-04-27 07:49:41 -0500

    Create punto2.py

---
+++ Evaluacion_3/punto2.py
@@ -0,0 +1,79 @@
+import copy
+
+def imprimirEcuaciones(a, b):
+    for i in range(len(a)):
+        for j in range(len(a[i])):
+            print(str(a[i][j]), end=" ")
+        
+        print("| " + str(b[i]))
+
+
+
+def gaussJordan(a, b):
+    aAux = copy.deepcopy(a)
+    bAux = copy.deepcopy(b)
+
+    n = len(bAux)
+
+    print("Ecuaciones")
+    imprimirEcuaciones(aAux, bAux)
+    print()
+
+    #Se construye la matriz identidad
+    
+
+    for i in range(n):
+        #Reducción
+        if(aAux[i][i]==0):
+                for x in range((i+1),n):
+                    if(aAux[x][i]!=0):
+                        temp1 = aAux[i]
+                        aAux[i]=aAux[x]
+                        aAux[x]=temp1
+                        temp1 = bAux[i]
+                        bAux[i]=bAux[x]
+                        bAux[x]=temp1
+                        break
+        for j in range(n):
+            
+            
+            
+            if i != j:
+                fact = -aAux[j][i] / aAux[i][i]
+
+                for k in range(n):
+                    aAux[j][k] += (aAux[i][k] * fact)
+
+                bAux[j] += (bAux[i] * fact)
+                
+            
+
+            print("Reducción " + str(i + 1))
+            imprimirEcuaciones(aAux, bAux)
+            print()
+    for i in range(n):
+                if (aAux[i][i]!=1):
+                    bAux[i] = bAux[i]/aAux[i][i]
+                    aAux[i][i] = aAux[i][i]/aAux[i][i]
+    return bAux
+
+
+#Programa principal
+a = [[2, 2, 0,-1], [5, 5, 3,-2], [2, 1, 3,-3],[0,5,-4,1]]
+b = [9, 3, -1,11]
+
+x = gaussJordan(a, b)
+
+#Se imprimen los resultados
+for i in range(len(x)):
+    print("x" + str(i + 1) + " = " + str(x[i]))
+
+print()
+
+#Se imprimen las pruebas
+for i in range(len(x)):
+    test = 0;
+    for j in range(len(x)):
+        test += a[i][j] * x[j]
+    test -= b[i]
+    print("test " + str(i + 1) + " = " + str(test))
